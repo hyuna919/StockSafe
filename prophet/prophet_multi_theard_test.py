3f1dafa3ff0c728a9d69c1d2ead54c1bc73bd8f2
hyuna919 <hyuna919@naver.com>
2022-07-28 00:51:18 +0900

    FEAT : 실거래 데이터를 캔들스틱 차트로 보여줌
    mplfinance 사용
    하나의 그래프에 실거래 데이터, 거래량, 예측치를 모두 보여준다.

--- prophet/prophet_multi_theard_test.py
+++ prophet/prophet_multi_theard_test.py
@@ -1,14 +1,18 @@
 # 필요 모듈...?매번 받는건 아닌것같은데
-import FinanceDataReader as fdr
-from prophet import Prophet
-from prophet.plot import plot_plotly, plot_components_plotly
-from prophet.plot import add_changepoints_to_plot
 import pymysql
 import os
 from google.cloud import storage
 import threading
 import time
 
+import FinanceDataReader as fdr
+from prophet import Prophet
+import pandas as pd
+import mplfinance as mpf
+
+
+TRADING_PERIODS = 1000
+PROPHET_PERIODS = 365
 
 def readStockList():
     # DB에서 주식코드 받아오기
@@ -42,30 +46,69 @@ def predict(list1):
         m.fit(data)
 
         # 기간 설정
-        future = m.make_future_dataframe(periods=365)
+        future = m.make_future_dataframe(periods=PROPHET_PERIODS)
         # 예측
         forecast = m.predict(future)
 
-        # flexibilty조절
-        # m = Prophet(changepoint_prior_scale=0.8)
-        # forecast = m.fit(data).predict(future)
-
+        
+        ########################
         # 예측 데이터 시각화
-        # 처음 80%의 시계열 데이터에 대하여 잠재적인 25개의 changepoints를 만들고, 그 중 선별하여 최종 changepoints를 그래프에서 vertical line으로 그려주게 됩니다.
-        # https://hyperconnect.github.io/2020/03/09/prophet-package.html
-        # https://zzsza.github.io/data/2019/02/06/prophet/
-        fig = m.plot(forecast)
+        #경향 그래프 그리기
+        # (Prophet 기능 사용, forecast데이터 자르기 전에 처리해야)
+        ########################
+        file_name = "prophet/img/" + STOCK_CODE + "_seasonality.png"
+        m.plot_components(forecast).savefig(file_name)
+                
+        ########################
+        # 예측 데이터 시각화
+        # 실거래+예상치 데이터 그리기
+        # 차트용으로 데이터 자르기
+        ########################
+        # 실거래데이터 자르기
+        data = data[-TRADING_PERIODS:]
+
+        # 예측치를 위한 실거래 데이터의 날짜가 더 길어야한다.
+        dummy = data[-1:]
+        tmp = forecast[-PROPHET_PERIODS:]
+        for i in range(PROPHET_PERIODS) :
+            dummy.index = tmp[i:i+1].ds
+            dummy.Close = 0
+            dummy.Open = 0
+            dummy.High = 0
+            dummy.Low = 0
+            dummy.Volume = 0
+            dummy.Change = 0
+            dummy.y = 0
+            dummy.ds = 0
+            data = pd.concat([data,dummy])
+
+
+        # 예측치 자르기
+        forecast = forecast[(TRADING_PERIODS+PROPHET_PERIODS)*-1:]
+        forecast_t = forecast['ds'].dt.to_pydatetime()
+
+
+        # 차트 그리기(밑그림)
+        index = data.index.astype('str') # 캔들스틱 x축이 str로 들어감
+
+        # 예측선 그리기
+        ap0 = [ mpf.make_addplot(forecast['yhat'],color='g',type='line') ]
 
         file_name1 = "prophet/img/" + STOCK_CODE + ".png"
-        graph = m.plot(forecast).savefig(file_name1)
-        # add_changepoints_to_plot(fig.gca(), m, forecast)
+        save = dict(fname=file_name,dpi=100,pad_inches=1)
+        mpf.plot(data,                # 차트에 채울 데이터(실거래가)
+                addplot=ap0,         # 추가로 그릴 데이터(자체적으로 테마 등 지정 되어있음)
+                volume=True,         # 거래량
+                style='yahoo',       # 차트 테마(색,선, ...)
+                type='candle',       # 그래프 타입 : 캔들스틱
+                savefig=save,        # 이미지로 저장
+                figratio=(18,10),     # 차트의 종횡비
+                ylabel='Price($)',
+                ylabel_lower='Volume'
+                # ylabel='가격(달러)',
+                # ylabel_lower='거래량'
+                )
 
-        file_name2 = "prophet/img/" + STOCK_CODE + "_seasonality.png"
-        graph_seasonality = m.plot_components(forecast).savefig(file_name2)
-
-        # m.close(fig)
-        # fig.close(graph)
-        # fig.close(graph_seasonality)
         gcpUpload(file_name1=file_name1)
 
     return [file_name1, file_name2]
