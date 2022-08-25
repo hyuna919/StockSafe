9d901d5aba631254c7f24d9afb5700f370b3cac1
simba155korea <simba155korea@gmail.com>
2022-08-25 14:58:40 +0900

    FEAT :  log4j 이용하여 로그 기능 추가

--- backend/src/main/java/com/LJ/StockSafe/model/service/StockServiceImpl.java
+++ backend/src/main/java/com/LJ/StockSafe/model/service/StockServiceImpl.java
@@ -2,6 +2,9 @@ package com.LJ.StockSafe.model.service;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +13,8 @@ import com.LJ.StockSafe.model.mapper.StockMapper;
 
 @Service
 public class StockServiceImpl implements StockService{
+
+	private Logger logger = LogManager.getLogger(this.getClass());
 
 	@Autowired
 	StockMapper stockmapper;
@@ -25,11 +30,13 @@ public class StockServiceImpl implements StockService{
 	public List<StockDto> readStockAll(String keyword) {
 		// TODO Auto-generated method stub
 		
-		List<StockDto> temp = stockmapper.readStockAll(keyword);
+		List<StockDto> stockDtoList = stockmapper.readStockAll(keyword);
 		
-		for(int a=0; a<temp.size(); a++) System.out.println(temp.toString());
+		//결과 출력을 log4j를 이용해 출력.
+		logger.info(stockDtoList);
+		//for(int a=0; a<temp.size(); a++) System.out.println(temp.toString());
 				
-		return temp;
+		return stockDtoList;
 	}
 
 	@Override
